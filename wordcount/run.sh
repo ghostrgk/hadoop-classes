38b7416df0fa343c1dba86fe28daefb1fb266281
Roman <rgk.I@ya.ru>
2020-11-09 21:12:08 +0300

    wordcount run.sh

--- wordcount/run.sh
+++ wordcount/run.sh
@@ -3,4 +3,4 @@
 OUT_DIR="word_count_had2020008"
 mvn package
 hdfs dfs -rm -r -skipTrash ${OUT_DIR}/*
-yarn jar target/PageLifeCounter-1.0.jar org.atp.PageLifeCounter /data/wiki/en_articles ${OUT_DIR}
+yarn jar target/wordcount-1.0-SNAPSHOT.jar edu.phystech.wordcount.WordCount /data/wiki/en_articles ${OUT_DIR}
