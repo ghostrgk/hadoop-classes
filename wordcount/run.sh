72021806cc08aa6b87151c6963537c68474917ee
Roman <rgk.I@ya.ru>
2020-11-09 21:09:30 +0300

    wordcount run.sh

--- wordcount/run.sh
+++ wordcount/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUT_DIR="word_count"
+OUT_DIR="word_count_had2020008"
 mvn package
-hdfs dfs -rm -r -skipTrash ${OUT_DIR}*
+hdfs dfs -rm -r -skipTrash ${OUT_DIR}/*
 yarn jar target/PageLifeCounter-1.0.jar org.atp.PageLifeCounter /data/wiki/en_articles ${OUT_DIR}
