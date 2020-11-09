72f3d18422d5d228271a4b5f2e2e8eec476fb610
roponasenko <had2020008@mipt-client.atp-fivt.org>
2020-11-09 19:12:55 +0000

    run.sh refactored

--- wordcount/run.sh
+++ wordcount/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+IN_DIR="/data/wiki/en_articles_part"
 OUT_DIR="word_count_had2020008"
+
+JAR_NAME="target/wordcount-1.0-SNAPSHOT.jar"
+MAIN_CLASS="edu.phystech.wordcount.WordCount"
+
 mvn package
-hdfs dfs -rm -r -skipTrash ${OUT_DIR}/*
-yarn jar target/wordcount-1.0-SNAPSHOT.jar edu.phystech.wordcount.WordCount /data/wiki/en_articles ${OUT_DIR}
+
+hdfs dfs -rm -r -skipTrash ${OUT_DIR}*
+yarn jar ${JAR_NAME} ${MAIN_CLASS} ${IN_DIR} ${OUT_DIR}
+
